b927f2eb49f8144ce054de37241fb162c026dea1
pmb <pmb@heise.de>
2020-07-30 13:29:24 +0200

    chore: remove unused code

--- example/app.ts
+++ example/app.ts
@@ -6,15 +6,10 @@ import { Props } from './views/my-view'
 
 export const app = express()
 
-const extension = __filename.endsWith('.js') ? 'js' : 'tsx'
-
 setupReactViews(app, {
   viewsDirectory: resolve(__dirname, 'views'),
   prettify: true,
 })
-// app.set('view engine', extension)
-// app.engine(extension, reactViews({ prettify: true }))
-// app.set('views', resolve(__dirname, 'views'))
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   const data: Props = { title: 'Test', lang: 'de' }
