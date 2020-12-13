20ea5c9dd8bd2747890c38a8297d847866aa4590
pmb <pmb@heise.de>
2020-12-13 22:37:41 +0100

    fix: add missing `ReactViewsOptions` docs

--- src/react-view-engine.interface.ts
+++ src/react-view-engine.interface.ts
@@ -1,9 +1,29 @@
 import { Application } from 'express'
 
 export interface ReactViewsOptions {
-  doctype?: string
-  prettify?: boolean
+  /**
+   * The directory where your views (`.tsx` files) are stored. Must be
+   * specified.
+   */
   viewsDirectory: string
+
+  /**
+   * [Doctype](https://developer.mozilla.org/en-US/docs/Glossary/Doctype) to
+   * be used. */
+  doctype?: string
+
+  /**
+   * If activated, the generated HTML string is formatted using
+   * [prettier](https://github.com/prettier/prettier)
+   */
+  prettify?: boolean
+
+  /**
+   * With this optional function the rendered HTML document can be modified. For
+   * this purpose a function must be defined which gets the HTML `string` as
+   * argument. The function returns a modified version of the HTML string as
+   * `string`.
+   */
   transform?: (html: string) => string | Promise<string>
 }
 
